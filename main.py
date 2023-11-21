d236126ced06c21f851653cae1af14a8a6a8365e
NhanNguyen <nnguyen14@sfsu.edu>
2023-11-21 15:08:30 -0800

    random the run

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import time
 import random
 
@@ -19,49 +21,57 @@ def is_safe(board, row, col):
 
     return True
 
-def solve_nq_util(board, col):
+def solve_nq_monte_carlo(board, col, node_count):
     if col >= len(board):
         return True
 
+    children = []
     for i in range(len(board)):
         if is_safe(board, i, col):
-            board[i][col] = 1
-            if solve_nq_util(board, col + 1) == True:
-                return True
-            board[i][col] = 0
+            children.append(i)
+
+    random.shuffle(children)  # Shuffle to ensure random selection
+
+    for i in children:
+        board[i][col] = 1
+        node_count[0] += 1  # Increment node count
+        if solve_nq_monte_carlo(board, col + 1, node_count):
+            return True
+        board[i][col] = 0
 
     return False
 
-def solve_n_queens(n):
+def solve_n_queens_monte_carlo(n):
     board = [[0] * n for _ in range(n)]
-    if solve_nq_util(board, 0) == False:
-        return False
-
-    return board
-
-def print_board(board):
-    for row in board:
-        print(" ".join(str(x) for x in row))
+    node_count = [0]  # Initialize node count
+    if not solve_nq_monte_carlo(board, 0, node_count):
+        return False, node_count[0]
+    return board, node_count[0]
 
 def monte_carlo_simulation(runs, n):
     times = []
+    nodes_counts = []
     for _ in range(runs):
         start_time = time.time()
-        solve_n_queens(n)
+        solution, nodes = solve_n_queens_monte_carlo(n)
         end_time = time.time()
         times.append(end_time - start_time)
-    return times
+        nodes_counts.append(nodes)
+    return times, nodes_counts
 
+def plot_queens(board):
+    """Plot the positions of queens on the chess board."""
+    n = len(board)
+    grid = np.zeros((n, n))
 
-print('Monte Carlo simulation for solving 12-Queens problem')
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                grid[i, j] = 1
 
-# Calling the main function to solve the 12-Queens problem and print the solution board
-n_queens = 12
-solution_board = solve_n_queens(n_queens)
-
-# Printing the solution board if a solution exists
-if solution_board:
-    for row in solution_board:
-        print(" ".join(str(x) for x in row))
-else:
-    print("No solution found for the 12-Queens problem.")
+    plt.imshow(grid, cmap='gray', interpolation='none')
+    plt.grid(which='major', axis='both', linestyle='--', color='yellow', linewidth=2)
+    plt.xticks(np.arange(0, n, 1))
+    plt.yticks(np.arange(0, n, 1))
+    plt.title('12-Queens Solution')
+    plt.show()
